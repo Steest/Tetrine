88511b48f78da603d670506d7d8ba58b3af2c992
hlimbo <limboh27@gmail.com>
2016-06-26 22:17:59 -0700

    Uncomment code that randomizes next tetromino to spawn and set default level to Test.umap

--- Source/Tetrine/Possessor.cpp
+++ Source/Tetrine/Possessor.cpp
@@ -90,8 +90,8 @@ void APossessor::Tick(float DeltaTime)
 
 		CurrentTetromino = SpawnTetromino();
 		if (CurrentTetromino == nullptr) { UE_LOG(Possessor_log, Log, TEXT("Tetro2 == nullptr")); return; }
-		//CurrentTetromino->SpawnShape(GenerateRandomTetromino());
-		CurrentTetromino->SpawnShape("flat");
+		CurrentTetromino->SpawnShape(GenerateRandomTetromino());
+		//CurrentTetromino->SpawnShape("flat");
 		CurrentTetromino->MoveTetrominoOnGrid(FVector2D(0, 0), grid);
 		OldGhostPositions = CurrentTetromino->GetPositions();
 		NextTetromino = GenerateRandomTetromino();
@@ -127,7 +127,7 @@ ATetromino* APossessor::SpawnTetromino()
 
 FString APossessor::GenerateRandomTetromino()
 { // we know there are 7 different types of blocks
-	return "flat";
+	//return "flat";
 	float randy = FMath::RandRange(0.0f, 1.0f); // break up into evenly random block probabilities
 	if (randy < .143) { return "square"; }
 	else if (randy < .143 * 2) { return "right bicep"; }
