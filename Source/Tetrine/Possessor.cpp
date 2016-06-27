b6109d9ea126410a672c68e20949526db95b5139
steest <steveestrada@live.com>
2016-06-26 21:37:23 -0700

    Fixed instant drop row deletion issue
    Added code to ensure that instant dropping would clear rows when rows
    are filled.

--- Source/Tetrine/Possessor.cpp
+++ Source/Tetrine/Possessor.cpp
@@ -79,7 +79,7 @@ void APossessor::Tick(float DeltaTime)
 		if (CurrentHorizontalMove != 0.0f) { UpdateHorizontalElapsed(DeltaTime);  bHasChangedPositions = true;}
 		UpdateFallElapsed(DeltaTime);
 		if (bHasChangedPositions) { UpdateGhostTetromino(); }
-		if (bIsInstantDropped) { InstantDrop(); bHasTetrominoLanded = false; bIsInstantDropped = false; }
+		if (bIsInstantDropped) { InstantDrop(); bHasTetrominoLanded = true; bIsInstantDropped = false; LandedTimeElapsed = LandedTimeLimit; }
 		if (bHasTetrominoLanded) { UpdateLandedElapsed(DeltaTime); }
 	}
 	else
@@ -91,7 +91,7 @@ void APossessor::Tick(float DeltaTime)
 		CurrentTetromino = SpawnTetromino();
 		if (CurrentTetromino == nullptr) { UE_LOG(Possessor_log, Log, TEXT("Tetro2 == nullptr")); return; }
 		//CurrentTetromino->SpawnShape(GenerateRandomTetromino());
-		CurrentTetromino->SpawnShape("t");
+		CurrentTetromino->SpawnShape("flat");
 		CurrentTetromino->MoveTetrominoOnGrid(FVector2D(0, 0), grid);
 		OldGhostPositions = CurrentTetromino->GetPositions();
 		NextTetromino = GenerateRandomTetromino();
@@ -127,7 +127,7 @@ ATetromino* APossessor::SpawnTetromino()
 
 FString APossessor::GenerateRandomTetromino()
 { // we know there are 7 different types of blocks
-
+	return "flat";
 	float randy = FMath::RandRange(0.0f, 1.0f); // break up into evenly random block probabilities
 	if (randy < .143) { return "square"; }
 	else if (randy < .143 * 2) { return "right bicep"; }
@@ -362,9 +362,10 @@ void APossessor::InstantDrop()
 		grid->GetBlock(CurrentTetromino->blocks[i]->GetPosition())->SetBlockStatus(0);
 		grid->GetBlock(CurrentTetromino->blocks[i]->GetPosition())->SetBlockSprite(0);
 	
+		grid->GetBlock(OldGhostPositions[i])->SetBlockStatus(2);
+		grid->GetBlock(OldGhostPositions[i])->SetBlockSprite(2);
+
 		CurrentTetromino->blocks[i]->SetPosition(OldGhostPositions[i]);
 		CurrentTetromino->blocks[i]->SetActorLocation(CurrentTetromino->blocks[i]->GetDimensions().X * FVector(OldGhostPositions[i].X, 0, OldGhostPositions[i].Y));
 	}
-	CurrentTetromino->EndLife(grid);
-	CurrentTetromino = nullptr;
 }
